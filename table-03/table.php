0d0b2f3e551008a4d8abecd291187e91dcaa0030
rohancollage07 <87567474+rohancollage07@users.noreply.github.com>
2022-07-01 16:30:27 +0530

    mgm

---
+++ table-03/table.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Table 03</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container-fluid-sm ">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section"></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 col-sm-4">
+					<h4 class="text-center mb-4">Important Dates</h4>
+					<div class="table-wrap">
+						<table class="table">
+					    <thead class="thead-primary">
+					      <tr>
+					        <th>Program</th>
+					        <th>Dates</th>
+					      
+					      </tr>
+					    </thead>
+					    <tbody>
+					      <tr>
+					        <th scope="row" class="scope" >Full-Text Paper Submission Deadline</th>
+					        <td>Full-Text Paper Submission Deadline</td>
+					    
+					        
+					      </tr>
+					      <tr>
+					        <th scope="row" class="scope" >Notification of Acceptance/Rejection</th>
+					        <td>bvbv</td>
+					       
+					      
+					      </tr>
+					      <tr>
+					        <th scope="row" class="scope" >Final Paper (Camera Ready) Submission</th>
+					        <td>Final Paper (Camera Ready) Submission</td>
+					       
+					       
+					      </tr>
+					      <tr>
+					        <th scope="row" class="scope" >Late Registration</th>
+					        <td>1 Year</td>
+					        
+			
+					      </tr>
+					      <tr>
+					        <th scope="row" class="scope" >Conference Dates</th>
+					        <td>1 Year</td>
+					       
+					     
+					      </tr>
+					      
+					    </tbody>
+					  </table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
